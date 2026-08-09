d3351fce3c194dea17748e71465eb59aba4d3c63
Gb168168 <goldbricks168@gmail.com>
2026-08-10 02:23:02 +0800

    重新規劃桌面表單高資訊密度 Grid

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "csr-v14";
+const CACHE_NAME = "csr-v15";
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => clients.claim()));
