3073c4e10251ad84f03d562ee841b9b13b7394ca
Gb168168 <goldbricks168@gmail.com>
2026-08-10 05:56:42 +0800

    修正 iPhone 第二排導覽仍被裁切

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "csr-v18";
+const CACHE_NAME = "csr-v19";
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => clients.claim()));
