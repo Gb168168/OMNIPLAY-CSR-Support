1d73d0ac05adecd2d1ad9033cb147c4040dcccb7
Gb168168 <goldbricks168@gmail.com>
2026-08-11 07:56:38 +0800

    更新自動全滿列表快取

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "csr-v42";
+const CACHE_NAME = "csr-v43";
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => clients.claim()));
