5a078c777a5dc56338e6aad27c68f4d19e9ec46e
Gb168168 <goldbricks168@gmail.com>
2026-08-11 06:56:55 +0800

    修正交接頁超出網格上限與檢視列失效 (#109)
    * fix: fit handover layout within grid limits
    
    * style: retain explicit handover rows in view mode
    
    * chore: refresh handover grid cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "csr-v30";
+const CACHE_NAME = "csr-v31";
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => clients.claim()));
